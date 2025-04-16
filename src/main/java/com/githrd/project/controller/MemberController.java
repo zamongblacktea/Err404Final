9db1981cc3a5bc056b8f19ea8576c1fcfaa9023d
taeseop park <okaqua91@gmail.com>
2025-04-16 10:02:28 +0900

    fix MemberController.java divide member / owner / rider

--- src/main/java/com/githrd/project/controller/MemberController.java
+++ src/main/java/com/githrd/project/controller/MemberController.java
@@ -58,16 +58,7 @@ public class MemberController {
 	private final NaverServiceImpl naverService;
 	private final KakaoServiceImpl kakaoService;
 
-	@RequestMapping("list.do")
-	public String list(Model model) {
 
-		List<MemberVo> list = memberMapper.selectList();
-
-		// 결과적으로 request binding
-		model.addAttribute("list", list);
-
-		return "member/member_list";
-	}
 
 	// 로그인 폼 띄우기(SNS 로그인 연동)
 	@RequestMapping("login_form.do")
@@ -138,6 +129,155 @@ public class MemberController {
 			return "redirect:" + url; // 원래있던 페이지로 이동시켜라
 	}
 
+	// 로그아웃
+	@RequestMapping("logout.do")
+	public String logout() {
+
+		// 해당세션에 있는 user정보 삭제
+		session.removeAttribute("user");
+
+		return "redirect:../main/main.do";
+	}
+
+	// 네이버 간편회원가입폼 띄우기
+	@RequestMapping("insert_form.do")
+	public String insert_form(Model model, String naver_id, long mem_id) throws Exception {
+
+		// SNS 연동된 정보 가져오기
+		// NaverVo naverInfo = naverService.getNaverInfo(naver_id);
+		model.addAttribute("mem_id", mem_id);
+		model.addAttribute("naver_id", naver_id);
+		return "member/member_insert_form";
+	}
+
+	// 카카오 간편 회원가입폼 띄우기
+	@RequestMapping("insert_kakao.do")
+	public String insert_kakao(Model model, long mem_id) throws Exception {
+
+		// SNS 연동된 정보 가져오기
+		model.addAttribute("mem_id", mem_id);
+
+		return "member/member_insert_form_kakao";
+	}
+
+
+	// 일반회원 가입
+	@RequestMapping("insert.do")
+	public String insert(MemberVo vo) {
+
+		System.out.println(vo);
+		// 1.회원가입자의 IP구하기
+		String mem_ip = request.getRemoteAddr();
+		vo.setMem_ip(mem_ip);
+
+		// 2.DB insert
+		int res = memberMapper.insert(vo);
+
+		return "redirect:member/list.do";
+	}
+
+	// 수정폼 띄우기
+	// /member/modify_form.do?mem_idx=2
+	@RequestMapping("modify_form.do")
+	public String modify_form(int mem_idx, Model model) {
+
+		// 1.mem_idx에 대한 MemberVo 구한다
+		MemberVo vo = memberMapper.selectOneFromIdx(mem_idx);
+
+		// 3.request binding
+		model.addAttribute("vo", vo);
+
+		return "member/member_modify_form";
+	}
+
+	// 수정하기
+	@RequestMapping("modify.do")
+	public String modify(MemberVo vo) {
+
+		// IP구해서 vo에 넣기
+		String mem_ip = request.getRemoteAddr();
+		vo.setMem_ip(mem_ip);
+
+		// 수정
+		int res = memberMapper.update(vo);
+
+		// 현재 수정한 멤버가 로그인한 멤버냐?
+		MemberVo user = (MemberVo) session.getAttribute("user");
+
+		if (user.getMem_idx() == vo.getMem_idx()) {
+			// 수정된 정보를 다시 얻어온다
+			user = memberMapper.selectOneFromIdx(vo.getMem_idx());
+			// 가져온 정보를 세션에 다시 넣는다
+			session.setAttribute("user", user);
+		}
+
+		return "redirect:list.do";
+	}
+
+	// 회원 탈퇴
+	@RequestMapping("delete.do")
+	public String delete(int mem_idx) {
+
+		int res = memberMapper.delete(mem_idx);
+
+		return "redirect:list.do";
+	}
+
+////////////////////////////////////////////////////////////////// 사장님//////////////////////////////////////////////
+
+
+
+	// 사장님 회원 가입
+	@RequestMapping("insert_owner.do")
+	public String insert_owner(OwnerVo vo) {
+
+		// 1.회원가입자의 IP구하기
+		String owner_ip = request.getRemoteAddr();
+		vo.setOwner_ip(owner_ip);
+
+		// 입력값 확인용 sysprint
+		System.out.println(vo);
+		// 2.DB insert
+		int res = ownerMapper.insert(vo);
+
+		return "redirect:login_form.do";
+	}
+
+	// 사장님 회원가입 폼 띄우기
+	@RequestMapping("owner_form.do")
+	public String insert_form_owner() throws Exception {
+
+		return "member/member_owner_form";
+	}
+
+	// 사장님 중복아이디 체크
+	@RequestMapping("check_id_owner.do")
+	// @ResponseBody : 응답데이터를 DS가 직접 응답해라
+	@ResponseBody
+	public Map<String, Boolean> check_id(String owner_id) {
+
+		// 1.owner_id에 해당되는 사용자정보 얻어오기
+		OwnerVo vo = ownerMapper.selectOneFromId(owner_id);
+
+		// 2.사용유무 판단
+		boolean bResult = false;
+
+		// 현재 아이디에 대한 회원이 없으면 사용가능하다 설정
+		if (vo == null)
+			bResult = true;
+
+		// {"result" : true} or {"result" : false}
+		Map<String, Boolean> map = new HashMap<String, Boolean>();
+		// key value
+		// String Boolean
+		map.put("result", bResult);
+
+		// Map을 JSON으로 반환시켜주는 객체(jsonConverter:context-7-jsonconverter.xml)
+		// DS이 받아서 MappingJackson2HttpMessageConverter에 지시
+		// 반환데이터(자바의 모든 데이터)를 JSON객체로 생성해서 반환해준다
+		return map;
+	}
+
 	// 사장님 로그인
 	@RequestMapping("owner_login.do")
 	public String ownerLogin(String owner_id, String owner_pwd,
@@ -192,7 +332,58 @@ public class MemberController {
 				return "redirect:../shop/main.do"; // 주문 현황 페이지
 			}
 		}
+	}//end: owner_login
+
+	// 사장님 아이디 비밀번호 찾기 페이지
+	@RequestMapping("owner_find.do")
+	public String findOwner() {
+
+		return "member/member_find_owner";
 	}
+
+	// 사장님 회원 탈퇴
+	@RequestMapping("delete_owner.do")
+	public String delete_owner(int owner_idx) {
+
+		int res = ownerMapper.delete(owner_idx);
+
+		return "redirect:list.do";
+	}// end : delete_owner
+
+////////////////////////////////////////////////////////////////////// 라이더//////////////////////////////////////////////////////
+
+	// 라이더 회원가입 폼 띄우기
+	@RequestMapping("rider_form.do")
+	public String insert_form_rider() throws Exception {
+
+		return "member/member_rider_form";
+	}//end: rider_form
+
+
+	// 라이더 회원 가입
+	@RequestMapping("insert_rider.do")
+	public String insert_rider(RiderVo vo) {
+
+		// 1.회원가입자의 IP구하기
+		String rider_ip = request.getRemoteAddr();
+		vo.setRider_ip(rider_ip);
+
+		// 입력값 확인용
+		System.out.println(vo);
+		// 2.DB insert
+		int res = riderMapper.insert(vo);
+
+		return "redirect:list.do";
+	}//end : insert_rider
+
+	// 라이더 회원 탈퇴
+	@RequestMapping("delete_rider.do")
+	public String delete_rider(int rider_idx) {
+
+		int res = riderMapper.delete(rider_idx);
+
+		return "redirect:list.do";
+	}// end: delete_rider
 
 	// 라이더 로그인
 	@RequestMapping("rider_login.do")
@@ -240,255 +431,53 @@ public class MemberController {
 			return "redirect:../rider/main.do"; // 메인화면 이동시켜라
 		else
 			return "redirect:" + url; // 원래있던 페이지로 이동시켜라
-	}
+	}//end : rider_login
 
-	// 로그아웃
-	@RequestMapping("logout.do")
-	public String logout() {
-
-		// 해당세션에 있는 user정보 삭제
-		session.removeAttribute("user");
-
-		return "redirect:../main/main.do";
-	}
-
-	// 네이버 간편회원가입폼 띄우기
-	@RequestMapping("insert_form.do")
-	public String insert_form(Model model, String naver_id, long mem_id) throws Exception {
-
-		// SNS 연동된 정보 가져오기
-		// NaverVo naverInfo = naverService.getNaverInfo(naver_id);
-		model.addAttribute("mem_id", mem_id);
-		model.addAttribute("naver_id", naver_id);
-		return "member/member_insert_form";
-	}
-
-	// 카카오 간편 회원가입폼 띄우기
-	@RequestMapping("insert_kakao.do")
-	public String insert_kakao(Model model, long mem_id) throws Exception {
-
-		// SNS 연동된 정보 가져오기
-		model.addAttribute("mem_id", mem_id);
-
-		return "member/member_insert_form_kakao";
-	}
-
-	// 사장님 회원가입 폼 띄우기
-	@RequestMapping("owner_form.do")
-	public String insert_form_owner() throws Exception {
-
-		return "member/member_owner_form";
-	}
-
-	// 라이더 회원가입 폼 띄우기
-	@RequestMapping("rider_form.do")
-	public String insert_form_rider() throws Exception {
-
-		return "member/member_rider_form";
-	}
-
-	// 중복아이디 체크
-	@RequestMapping("check_id.do")
-	// @ResponseBody : 응답데이터를 DS가 직접 응답해라
-	@ResponseBody
-	public Map<String, Boolean> check_id(String owner_id) {
-
-		// 1.owner_id에 해당되는 사용자정보 얻어오기
-		OwnerVo vo = ownerMapper.selectOneFromId(owner_id);
-
-		// 2.사용유무 판단
-		boolean bResult = false;
-
-		// 현재 아이디에 대한 회원이 없으면 사용가능하다 설정
-		if (vo == null)
-			bResult = true;
-
-		// {"result" : true} or {"result" : false}
-		Map<String, Boolean> map = new HashMap<String, Boolean>();
-		// key value
-		// String Boolean
-		map.put("result", bResult);
-
-		// Map을 JSON으로 반환시켜주는 객체(jsonConverter:context-7-jsonconverter.xml)
-		// DS이 받아서 MappingJackson2HttpMessageConverter에 지시
-		// 반환데이터(자바의 모든 데이터)를 JSON객체로 생성해서 반환해준다
-		return map;
-	}
-
-	// 일반회원 가입
-	@RequestMapping("insert.do")
-	public String insert(MemberVo vo) {
-
-		System.out.println(vo);
-		// 1.회원가입자의 IP구하기
-		String mem_ip = request.getRemoteAddr();
-		vo.setMem_ip(mem_ip);
-
-		// 2.DB insert
-		int res = memberMapper.insert(vo);
-
-		return "redirect:member/list.do";
-	}
-
-	// 사장님 회원 가입
-	@RequestMapping("insert_owner.do")
-	public String insert_owner(OwnerVo vo) {
-
-		// 1.회원가입자의 IP구하기
-		String owner_ip = request.getRemoteAddr();
-		vo.setOwner_ip(owner_ip);
-
-		// 입력값 확인용 sysprint
-		System.out.println(vo);
-		// 2.DB insert
-		int res = ownerMapper.insert(vo);
-
-		return "redirect:login_form.do";
-	}
-
-	// 라이더 회원 가입
-	@RequestMapping("insert_rider.do")
-	public String insert_rider(RiderVo vo) {
-
-		// 1.회원가입자의 IP구하기
-		String rider_ip = request.getRemoteAddr();
-		vo.setRider_ip(rider_ip);
-
-		// 입력값 확인용
-		System.out.println(vo);
-		// 2.DB insert
-		int res = riderMapper.insert(vo);
-
-		return "redirect:list.do";
-	}
-
-	// 수정폼 띄우기
-	// /member/modify_form.do?mem_idx=2
-	@RequestMapping("modify_form.do")
-	public String modify_form(int mem_idx, Model model) {
-
-		// 1.mem_idx에 대한 MemberVo 구한다
-		MemberVo vo = memberMapper.selectOneFromIdx(mem_idx);
-
-		// 3.request binding
-		model.addAttribute("vo", vo);
-
-		return "member/member_modify_form";
-	}
-
-	// 수정하기
-	@RequestMapping("modify.do")
-	public String modify(MemberVo vo) {
-
-		// IP구해서 vo에 넣기
-		String mem_ip = request.getRemoteAddr();
-		vo.setMem_ip(mem_ip);
-
-		// 수정
-		int res = memberMapper.update(vo);
-
-		// 현재 수정한 멤버가 로그인한 멤버냐?
-		MemberVo user = (MemberVo) session.getAttribute("user");
-
-		if (user.getMem_idx() == vo.getMem_idx()) {
-			// 수정된 정보를 다시 얻어온다
-			user = memberMapper.selectOneFromIdx(vo.getMem_idx());
-			// 가져온 정보를 세션에 다시 넣는다
-			session.setAttribute("user", user);
-		}
-
-		return "redirect:list.do";
-	}
-
-	// 회원 탈퇴
-	@RequestMapping("delete.do")
-	public String delete(int mem_idx) {
-
-		int res = memberMapper.delete(mem_idx);
-
-		return "redirect:list.do";
-	}
-
-	// 사장님 회원 탈퇴
-	@RequestMapping("delete_owner.do")
-	public String delete_owner(int owner_idx) {
-
-		int res = ownerMapper.delete(owner_idx);
-
-		return "redirect:list.do";
-	}// end : delete_owner
-
-	// 라이더 회원 탈퇴
-	@RequestMapping("delete_rider.do")
-	public String delete_rider(int rider_idx) {
-
-		int res = riderMapper.delete(rider_idx);
-
-		return "redirect:list.do";
-	}// end: delete_rider
-
-	//라이더 아이디 비밀번호 찾기 페이지 
+	// 라이더 아이디 비밀번호 찾기 페이지
 	@RequestMapping("rider_find.do")
 	public String findRider() {
-		
-		
+
 		return "member/member_find_rider";
 	}
 
-	//사장님 아이디 비밀번호 찾기 페이지 
-	@RequestMapping("owner_find.do")
-	public String findOwner() {
-		
-		
-		return "member/member_find_owner";
-	}
-
-	//라이더 아이디 찾기
+	// 라이더 아이디 찾기
 	@RequestMapping("rider_verifyId.do")
-	public String riderVerfyId(@RequestParam String rider_email,Model model) {
-		
+	public String riderVerfyId(@RequestParam String rider_email, Model model) {
 
-		//이메일로 조회
+		// 이메일로 조회
 		RiderVo rider = riderMapper.selectOneFindInfo(rider_email);
 
-		//라이더 아이디 가져오기
+		// 라이더 아이디 가져오기
 		String rider_id = rider.getRider_id();
 
 		model.addAttribute("rider_id", rider_id);
-	
+
 		return "member/member_rider_verify_id";
 	}
 
-
-	//라이더 비밀번호 변경 페이지
+	// 라이더 비밀번호 변경 페이지
 	@RequestMapping("rider_verifyPwd.do")
-	public String riderChangePwd(@RequestParam String rider_email,Model model) {
-		
-	
+	public String riderChangePwd(@RequestParam String rider_email, Model model) {
+
 		return "member/member_rider_verify_pwd";
 	}
 
-
-	//라이더 비밀번호 변경
+	// 라이더 비밀번호 변경
 	@RequestMapping("rider_changePwd.do")
 	public String riderVerifyPwd(@RequestParam String rider_email,
-								 @RequestParam String rider_pwd,
-								 RiderVo vo) {
-		
+			@RequestParam String rider_pwd,
+			RiderVo vo) {
 
-
-		//이메일로 조회
+		// 이메일로 조회
 		RiderVo rider = riderMapper.selectOneFindInfo(rider_email);
 
 		// IP구해서 vo에 넣기
 		String rider_ip = request.getRemoteAddr();
 		vo.setRider_ip(rider_ip);
 
+		int res = riderMapper.pwdUpdate(vo); // 비밀번호 update
 
-		int res = riderMapper.pwdUpdate(vo); //비밀번호 update
-
-		return "redirect:login_form.do";//로그인 화면으로 redirect
-	}//end: rider_changePwd
-	
+		return "redirect:login_form.do";// 로그인 화면으로 redirect
+	}// end: rider_changePwd
 
 }// end: class memberController
