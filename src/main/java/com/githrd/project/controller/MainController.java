d9c238a3d52a8772f4cbaeec6dcf156e4cd52f27
taeseop park <okaqua91@gmail.com>
2025-04-23 16:05:27 +0900

    pull cart2

--- src/main/java/com/githrd/project/controller/MainController.java
+++ src/main/java/com/githrd/project/controller/MainController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.githrd.project.dao.ShopInfoMapper;
 import com.githrd.project.service.ShopService;
 import com.githrd.project.vo.ShopInfoVo;
+import com.githrd.project.vo.ShopMenuVo;
 
 @Controller
 @RequestMapping("/main")
